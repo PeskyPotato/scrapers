dd1e33ac0a60c151efdebb68926af762fd562378
LameLemon <peskypotato@protonmail.com>
2020-08-22 18:02:16 +0100

    🎞️  Updated impaward scraper
    - Added start and end arguments
    - Updated for new site

--- impawards/README.md
+++ impawards/README.md
@@ -1,25 +1,38 @@
-# wallhaven-scraper
+# impaward-scraper
 
-Downloads all available movie posters.
+Downloads all available movie posters from [IMP Awards](http://www.impawards.com/)
 
 ## Prerequisites
-* python3
-* bs4
+Install python3 requirements
+```
+pip3 install -r requirements.txt
+```
 
 ## Usage
+### All available arguments
+```bash
+$ python3 impawards-scraper.py -h
+usage: impawards-scraper.py [-h] [--start START] [--end END] [-o OUTPUT]
+
+Archive posters from impawards.com
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --start START         Starting pages number
+  --end END             Ending page number
+  -o OUTPUT, --output OUTPUT
+                        Set download directory
+
+```
+
+### Downloads all available posters
 ```
 python3 impawards-scraper.py 
 ```
 
-Sample output
-```
-memes@pepe:~/git/scrapers/wallhaven-scraper$  python3 impawards-scraper.py 
-==== Page 1  ====
-Aardvark (2018)
-Aaron's Blood (2017)
-Aaron's Blood (2017)
-Aaron Loves Angela (1975)
-Aatsinki: The Story of Arctic Cowboys (2014)
-
+### Downloads a range from alphabetically
+The pages can be viewed on the [site](http://www.impawards.com/alpha1.html).
+```bash
+$ python3 impawards-scraper.py --start 1 --end 10
 ```
 
--- impawards/impawards-scraper.py
+++ impawards/impawards-scraper.py
@@ -1,47 +1,93 @@
 from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen
-from urllib.request import Request
-from urllib.request import urlretrieve
-from urllib.error import HTTPError
-from time import sleep
+import requests
+import re
+from multiprocessing.pool import ThreadPool
 import os
-import sys
+import argparse
 
-def scrape():
+HEADERS = {
+    'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0"
+}
+BASE_URL = "http://www.impawards.com"
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-    page = 1
-    while(1):
-        print("==== Page", page, " ====")
-        url = "http://www.impawards.com/alpha{}.html".format(page)
 
-        req = Request(
-            url, 
-            data=None, 
-            headers={
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-            }
-        )
+def scrape(page):
+    print("Page", page)
+    url = BASE_URL + "/alpha{}.html".format(page)
+    page_html = requests.get(url, headers=HEADERS).content
+    page_soup = soup(page_html, "html5lib")
 
+    posters = page_soup.findAll("div", {"class": "constant_thumb"})
+    posters_dl = []
+    for poster in posters:
+        src = poster.center.a.img["src"].replace("med_", "")
+        src_xlg = src.replace(".jpg", "_xlg.jpg")
+        src_xxlg = src.replace(".jpg", "_xxlg.jpg")
+        year = re.search(r"\(([\d]+)\)$", poster.div.text).group(1)
+        file_dir = os.path.join(BASE_DIR, "posters", year)
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+        posters_dl.append((os.path.join(file_dir, src.split("/")[-1]), BASE_URL + src))
+        posters_dl.append((os.path.join(file_dir, src_xlg.split("/")[-1]), BASE_URL + src_xlg))
+        posters_dl.append((os.path.join(file_dir, src_xxlg.split("/")[-1]), BASE_URL + src_xxlg))
+
+    if not posters_dl:
+        return False
+
+    with ThreadPool(processes=8) as tp:
+        for imap_result in tp.imap_unordered(download_file, posters_dl):
+            pass
+    return True
+
+
+def download_file(data):
+    local_filename = data[0]
+    url = data[1]
+    if os.path.isfile(local_filename):
+        print("{:15}{}".format("Skipped", local_filename.split("/")[-1]))
+        return local_filename
+
+    try:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            with open(local_filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    except requests.exceptions.HTTPError:
+        print("{:15}{}".format("Not found", local_filename.split("/")[-1]))
+        return False
+
+    print("{:15}{}".format("Downloaded", local_filename.split("/")[-1]))
+    return local_filename
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Archive posters from impawards.com")
+    parser.add_argument("--start", type=int, help="Starting pages number", default=1)
+    parser.add_argument("--end", type=int, help="Ending page number", default=-1)
+    parser.add_argument("-o", "--output", type=str, help="Set download directory")
+    args = parser.parse_args()
+
+    global BASE_DIR
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+    if args.output:
+        BASE_DIR = os.path.abspath(args.output)
+        if not os.path.exists(BASE_DIR):
+            os.makedirs(BASE_DIR)
+
+    page = args.start
+    page_end = args.end
+    stop = True
+    while(stop):
         try:
-            uClient = urlopen(req)
-            page_html = uClient.read()
-            uClient.close()
-        except HTTPError:
-            print("Complete")
-            break
-        page_soup = soup(page_html, "lxml")
-
-        posters = page_soup.findAll("div", {"class": "constant_thumb"})
-
-        for poster in posters:
-            src = poster.center.a.img["src"]
-            name = poster.div.text
-            print(name)
-            urlretrieve("http://www.impawards.com{}".format(src), "posters/{}-{}".format(name.replace(" ", "_"), src.split("/")[-1]))
+            stop = scrape(page)
+        except requests.exceptions.ConnectionError:
+            stop = scrape(page)
         page += 1
+        if page_end != -1 and page > page_end:
+            stop = False
 
 
-if not os.path.exists('posters'):
-    os.makedirs('posters')
-
-scrape()
+if __name__ == "__main__":
+    main()
