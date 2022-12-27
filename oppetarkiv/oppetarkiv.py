28e708d2be2f92fa7754915e6e86c4bc7258a0c9
LameLemon <peskypotato@protonmail.com>
2022-12-27 19:47:20 +0100

    📽️ Updated OppetArkiv scraper
    - Updated for new SVT player front-end

--- oppetarkiv/oppetarkiv.py
+++ oppetarkiv/oppetarkiv.py
@@ -8,7 +8,7 @@ import os
 import json
 import argparse
 
-VALID_URL = "https://www.oppetarkiv.se"
+VALID_URL = "https://www.svtplay.se"
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
@@ -36,79 +36,76 @@ def format_name(title):
         title = title[:120]
     return title
 
-def single_episode(path):
-    print("Episode:", path)
+def single_episode(episode_data):
+    print("Episode:", episode_data["title"])
+    episode_title = episode_data["title"]
+    title_clean = format_name(episode_title)
 
-    page_html = requests.get(VALID_URL + path).content
-    page_soup = soup(page_html, "html.parser")
-
-    data_list = page_soup.find("dl", {"class": "svtoa-data-list"})
-    dd_list = data_list.findChildren("dd")
-    row = 0
-    ep_data = {}
-    for data in data_list.findChildren("dt"):
-        data_text = data.text
-        if data_text == "Titel:":
-            ep_data['titel'] = dd_list[row].a.text
-        elif data_text == "Genre:":
-            ep_data['genre'] = dd_list[row].a.text
-        elif data_text == "år:":
-            ep_data['year'] = dd_list[row].a.text
-        elif data_text == "Övrigt:":
-            ep_data['övrigt'] = dd_list[row].a.text
-        row += 1
-    
-    episode_title = page_soup.find("span", {"class": "svt-heading-s svt-display-block"})
-    if episode_title:
-        ep_data["avsnitt_titel"] = episode_title.text.strip()
-    
-    data =  page_soup.find("span", {"class": "svt-video-meta"}).findChildren("strong")
-    if len(data) == 1:
-        if "min" in data[0].text or "sek" in data[0].text:
-            ep_data["längd"] = data[0].text
-            ep_data["sändes"] = "Saknar sändningsdatum"
-    elif len(data) == 2:
-        ep_data["sändes"] = data[0].time['datetime']
-        ep_data["längd"] = data[1].text
-
-    description = page_soup.find("div", {"class": "svtContainerMain-Alt svtContainerMain-Alt--top-border svt-margin-bottom-0px"})
-    ep_data["beskrivning"] = str(description)
-
-    ep_id = re.search(r'\/video\/(?P<ep_id>[0-9]*)\/?', path).group('ep_id')
-    ep_data['id'] = ep_id
-
-    episode_title = ep_data.get("avsnitt_titel", "Ingen_avsnitt")
-    title_clean = format_name(ep_data.get("titel", "Ingen_showtitel"))
-
-    directory = os.path.join(BASE_DIR, title_clean)
+    directory = os.path.join(
+        BASE_DIR,
+        format_name(episode_data["program"]),
+        format_name(episode_data["season"]),
+        title_clean
+    )
     if not os.path.exists(directory):
         os.makedirs(directory)
-    with open(os.path.join(directory, format_name("{}-{}.json".format(ep_id, episode_title))), 'w') as f:
-        json.dump(ep_data, f)
-
+    with open(os.path.join(directory, format_name("{}-{}.json".format(episode_data["id"], episode_title))), 'w') as f:
+        json.dump(episode_data, f)
     ydl_opts = {
         'progress_hooks': [progress_hook],
         'logger': MyLogger(),
         'continuedl': True,
         'ignoreerrors': True,
-        'outtmpl': os.path.join(directory, format_name("{}-{}.%(ext)s".format(ep_id, episode_title)))
+        'outtmpl': os.path.join(directory, format_name("{}-{}.%(ext)s".format(episode_data["id"], episode_title)))
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([VALID_URL+path])
+        ydl.download([episode_data["url"]])
 
 def single_program(path, episodes=[], page=1):
-    # print("Path: {} | Episodes: {} | Page: {}".format(path, episodes, page))
-    url = VALID_URL + path + "?sida={}&sort=tid_stigande?embed=true".format(page)
+    url = VALID_URL + path 
     r = requests.get(url)
     if r.status_code == 404:
         return episodes
     page_html = r.content
     page_soup = soup(page_html, "html.parser")
 
-    current_episodes = page_soup.find_all("a", {"class": "svtLink-Discreet-THEMED svtJsLoadHref"})
-    for episode in current_episodes:
-        episodes.append(episode["href"])
-    return single_program(path, episodes=episodes, page= page + 1)
+    raw_data = page_soup.find("script", {"id": "__NEXT_DATA__"}).text
+    data = json.loads(raw_data)
+
+    series_info_key = ""
+    series_info_keys = list(data["props"]["urqlState"].keys())
+    for key in series_info_keys:
+        json_data = json.loads(data["props"]["urqlState"][key]["data"])
+        if not json_data.get("detailsPageByPath"):
+            continue
+        else:
+            series_info_key = key
+
+    series_info = data["props"]["urqlState"][series_info_key]["data"]
+
+    series_info_json = json.loads(series_info)
+    print(series_info_json["detailsPageByPath"].keys())
+
+    with open("test.json", "w")as f:
+        json.dump(series_info_json["detailsPageByPath"], f)
+
+    seasons = series_info_json["detailsPageByPath"]["associatedContent"]
+    for season in seasons:
+        print("SEASON:", season["name"])
+        current_episodes = season["items"]
+
+        for episode in current_episodes:
+            #print(episode["heading"], episode["item"]["urls"]["svtplay"])
+            ep_data = {
+                "id": episode["item"]["id"],
+                "program": series_info_json["detailsPageByPath"]["heading"],
+                "title": episode["heading"],
+                "season": season["name"],
+                "description": episode["description"],
+                "url": VALID_URL + episode["item"]["urls"]["svtplay"]
+            }
+            episodes.append(ep_data)
+    return episodes
 
 def all_programs():
     url = VALID_URL + "/program"
@@ -116,14 +113,16 @@ def all_programs():
     page_soup = soup(page_html, "html.parser")
 
     programs = []
-    temp_programs = page_soup.find_all("a", {"class": "svtoa-anchor-list-link"})
+    temp_programs = page_soup.find_all("a", {"class": "sc-73d4a946-4"})
     for program in temp_programs:
+        print("Adding program:", program["href"])
         programs.append(program["href"])
 
     return programs
 
 
 def scrape():
+    print("Gathering all programs...")
     programs = all_programs()
     for program in programs:
         episodes = single_program(program, episodes=[])
@@ -143,6 +142,9 @@ def main ():
             os.makedirs(BASE_DIR)
 
     scrape()
+    #episodes = single_program("/brus", episodes=[])
+    #for episode in episodes:
+    #    single_episode(episode)
 
 
 main()
